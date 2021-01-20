b52184a81accdb7eebb761494b4e2c78ddc986be
jamezahidalgo <76913109+jamezahidalgo@users.noreply.github.com>
2021-01-20 18:11:00 -0300

    Add files via upload

---
+++ sesion2/condicion.py
@@ -0,0 +1,10 @@
+x = int(input('Ingrese un número '))
+print("Valor ingresado =", x)
+if x > 0:
+    print("Valor ingresado es POSITIVO")
+else:
+    if x == 0:
+        print("Valor ingresado es CERO")
+    else:
+        print("Valor ingresado es NEGATIVO")
+print("Fin de mi programa")
